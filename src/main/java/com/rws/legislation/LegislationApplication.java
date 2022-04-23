d99a86b770d277e8541464e2a566cc6531f5052e
onrsgr <60180086+onrsgr@users.noreply.github.com>
2022-04-23 19:00:51 +0300

    Dependency Configured

--- src/main/java/com/rws/legislation/LegislationApplication.java
+++ src/main/java/com/rws/legislation/LegislationApplication.java
@@ -2,12 +2,16 @@ package com.rws.legislation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class LegislationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LegislationApplication.class, args);
+		System.out.println("Hello Rws");
+
+
 	}
 
 }
