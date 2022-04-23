95e285d72dab47e5db9ee3b3ab22d0eb1de30fc2
onrsgr <60180086+onrsgr@users.noreply.github.com>
2022-04-23 18:48:41 +0300

    Project Started

---
+++ src/main/java/com/rws/legislation/LegislationApplication.java
@@ -0,0 +1,13 @@
+package com.rws.legislation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegislationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegislationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rws/legislation/LegislationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rws.legislation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegislationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
